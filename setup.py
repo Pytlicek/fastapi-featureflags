3a55ea0342f9957bedf3503d85f9c38075202283
Tomas-Pytel <Tomas.Pytel@ibm.com>
2021-11-23 21:41:29 +0100

    Add requirements and project files

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="fastapi_featureflags",
+    version="0.1",
+    author="Tomas Pytel",
+    author_email="pytlicek@gmail.com",
+    description="Feature flags for FastAPI",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/Pytlicek/fastapi-featureflags",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["requests", "fastapi"],
+    python_requires=">=3.6",
+)
